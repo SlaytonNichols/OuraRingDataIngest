cd1b482f4adf79032b22d6f561b8533820b275fe
Slayton Nichols <nicholsslayton@gmail.com>
2022-10-08 20:22:29 -0400

    chore(*): cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-## Oura Ring Data Ingest
-
--- ui/src/types.ts
+++
@@ -1,5 +0,0 @@
-import { ViteSSGContext } from 'vite-ssg'
-
-export type UserModule = (ctx: ViteSSGContext) => void
-
-export type Crumb = { name: string, href: string }
